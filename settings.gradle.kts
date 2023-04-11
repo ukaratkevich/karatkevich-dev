d4265bc833dd08956a730109669120694b5585fc
vkorotkevich <vkorotkevich@damsols.com>
2023-04-11 21:37:41 +0200

    retured repositories block back into root build.gradle.kts since they're not appplied from included build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,19 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "karatkevichdev"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        mavenCentral()
+    }
 }
 
 includeBuild("build-logic")
