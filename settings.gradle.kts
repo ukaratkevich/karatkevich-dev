8161f3a86acf4d028ea49605de20498b447b3d3d
vkorotkevich <vkorotkevich@damsols.com>
2023-04-11 17:20:18 +0200

    removed repos for plugins and dependencies since included build share them

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,19 +3,8 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "karatkevichdev"
 
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-
-    repositories {
-        mavenCentral()
-    }
 }
 
 includeBuild("build-logic")
