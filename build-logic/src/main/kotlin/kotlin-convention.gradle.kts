40cab6b358006565b3585d83c9b21746e7e3e054
ukaratkevich <ukaratkevich@gmail.com>
2023-04-30 20:26:26 +0200

    - added coroutines to the project - added jvm plugin to the root build script - changed kotlin convention to use kotlin jvm version

--- build-logic/src/main/kotlin/kotlin-convention.gradle.kts
+++ build-logic/src/main/kotlin/kotlin-convention.gradle.kts
@@ -2,7 +2,7 @@ import gradle.kotlin.dsl.accessors._b8e9b34270198d14a4ec0d8967890a50.kotlin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin
+    kotlin("jvm")
 }
 
 kotlin {
