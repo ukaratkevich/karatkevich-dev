12238e82c78cb06c6497bd3d86fbe7274a97da11
ukaratkevich <ukaratkevich@gmail.com>
2023-05-05 19:22:51 +0200

    removed mutex from repository and replaced with a single threaded dispatcher

--- blog/src/main/kotlin/dev/karatkevich/articles/ArticlesRoutes.kt
+++ blog/src/main/kotlin/dev/karatkevich/articles/ArticlesRoutes.kt
@@ -8,11 +8,10 @@ import dev.karatkevich.articles.routes.postArticleRoute
 import dev.karatkevich.articles.routes.putArticleRoute
 import io.ktor.server.application.Application
 import io.ktor.server.routing.routing
-import kotlinx.coroutines.Dispatchers
 
 internal fun Application.articlesRouting() {
     routing {
-        val articlesRepository = InMemoryArticlesRepository(Dispatchers.IO)
+        val articlesRepository = InMemoryArticlesRepository()
 
         articleValidation()
 
--- blog/src/main/kotlin/dev/karatkevich/articles/model/InMemoryArticlesRepository.kt
+++ blog/src/main/kotlin/dev/karatkevich/articles/model/InMemoryArticlesRepository.kt
@@ -5,30 +5,26 @@ import dev.karatkevich.articles.domain.entities.Article
 import dev.karatkevich.articles.domain.entities.Id
 import dev.karatkevich.articles.domain.entities.isEmpty
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 internal class InMemoryArticlesRepository(
-    private val dispatcher: CoroutineDispatcher,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : ArticlesRepository {
 
+    private val dispatcher = dispatcher.limitedParallelism(1)
+
     private val articles = mutableListOf<Article>()
-    private val mutex = Mutex()
 
     override suspend fun getAll(): List<Article> {
-        return mutex.withLock {
-            withContext(dispatcher) {
-                articles.toList()
-            }
+        return withContext(dispatcher) {
+            articles.toList()
         }
     }
 
     override suspend fun getById(id: Id): Article? {
-        return mutex.withLock {
-            withContext(dispatcher) {
-                articles.find { it.id == id }
-            }
+        return withContext(dispatcher) {
+            articles.find { it.id == id }
         }
     }
 
@@ -41,48 +37,42 @@ internal class InMemoryArticlesRepository(
     }
 
     private suspend fun create(article: Article): Article {
-        return mutex.withLock {
-            withContext(dispatcher) {
-                articles += article
-                article
-            }
+        return withContext(dispatcher) {
+            articles += article
+            article
         }
     }
 
     private suspend fun update(article: Article): Article {
-        return mutex.withLock {
-            withContext(dispatcher) {
-                val existingArticle = articles.find { it.id == article.id }
+        return withContext(dispatcher) {
+            val existingArticle = articles.find { it.id == article.id }
 
-                val updatedArticle = existingArticle?.copy(
-                    title = article.title,
-                    description = article.description,
-                    cover = article.cover
-                ) ?: article
+            val updatedArticle = existingArticle?.copy(
+                title = article.title,
+                description = article.description,
+                cover = article.cover
+            ) ?: article
 
-                if (existingArticle != null) {
-                    articles.remove(existingArticle)
-                }
-
-                articles += updatedArticle
-
-                updatedArticle
+            if (existingArticle != null) {
+                articles.remove(existingArticle)
             }
+
+            articles += updatedArticle
+
+            updatedArticle
         }
     }
 
     override suspend fun delete(id: Id): Article? {
         val article = getById(id) ?: return null
 
-        return mutex.withLock {
-            withContext(dispatcher) {
-                val isRemoved = articles.remove(article)
+        return withContext(dispatcher) {
+            val isRemoved = articles.remove(article)
 
-                if (isRemoved) {
-                    article
-                } else {
-                    null
-                }
+            if (isRemoved) {
+                article
+            } else {
+                null
             }
         }
     }
