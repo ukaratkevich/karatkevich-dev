0bfe54bbce134b042e8f638f315f88bccebf5cd8
ukaratkevich <ukaratkevich@gmail.com>
2023-05-13 22:42:49 +0200

    - removed kotlin compile task set up for build script

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,22 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     `kotlin-dsl`
 }
 
 group = "build-logic"
-
-tasks.withType<KotlinCompile>().configureEach {
-    val kotlinLanguageVersion = libs.versions.kotlinLanguageVersionBuildScript.get()
-    val jvmTargetVersion = libs.versions.jvmTargetBuildScript.get()
-
-    kotlinOptions {
-        jvmTarget = jvmTargetVersion
-
-        languageVersion = kotlinLanguageVersion
-        apiVersion = kotlinLanguageVersion
-    }
-}
 
 dependencies {
     implementation(libs.kotlinPlugin)
